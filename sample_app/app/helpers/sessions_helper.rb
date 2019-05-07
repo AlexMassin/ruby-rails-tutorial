93733d4e853d8007e2e7507124c07e1189a44f6c
Alex Massin <aptmassin@gmail.com>
2019-05-07 14:45:22 -0400

    added cookies and sessions

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -5,13 +5,32 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+    if (user_id = session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+      elsif (user_id = cookies.signed[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          @current_user = user
+        end
+      end
+  end
+
+  def forget(user)
+    cookies.permanent.signed[:user_id] = nil
+    cookies.permanent[:remember_token] = nil
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def log_out
     session.delete(:user_id)
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
     @current_user = nil
   end
 
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -5,4 +5,25 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 250 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 } 
+    attr_accessor :remember_token
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+    
+      # Returns a random token.
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def authenticated?(remember_token)
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
 end
