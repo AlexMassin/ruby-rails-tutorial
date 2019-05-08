a07d5ef7f5a0cfc687989e4a85498016c18acb31
Alex Massin <aptmassin@gmail.com>
2019-05-08 10:09:06 -0400

    Added admin users and pagination

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin? 
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -6,10 +18,44 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_url
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # PATCH users/1
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  # POST users/1
   def create
     @user = User.new(user_params)
     if @user.save
-
       flash[:success] = "Welcome to the Sample App!"
       log_in @user
       redirect_to @user
--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -21,6 +21,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = nil
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
@@ -37,4 +41,16 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to (session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original.url if request.get?
+  end
+  
 end
